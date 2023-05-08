506d12b1e7a72c65b578fcb3820acb4ccd48dc08
LalehMashayekhi <132183349+LalehMashayekhi@users.noreply.github.com>
2023-05-08 20:39:52 +0330

    Add files via upload

---
+++ S4_randomNumbergame.py
@@ -0,0 +1,18 @@
+import random
+#game == random
+pc_number=random.randint(1,20)
+count=0
+while True:
+    user_number=int(input("Enter a number: "))
+    if user_number==pc_number:
+        print("Victoryyyy")
+        count+=1
+        print("You had : " , count ,"gusses")
+        break
+    elif user_number<pc_number and count<5:
+        print("Aim higher...")
+        count+=1
+    elif user_number>pc_number and count<5:
+        print("come downnn")
+        count+=1
+    
