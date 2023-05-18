cf309de8c190d5fd583f8cd3aacf72c58a67e5a5
LalehMashayekhi <132183349+LalehMashayekhi@users.noreply.github.com>
2023-05-18 17:41:54 +0330

    Update List of random numbers_S4_A3.py

--- List of random numbers_S4_A3.py
+++ List of random numbers_S4_A3.py
@@ -1,7 +1,7 @@
 import random
-number_in_list=int(input("Enter the count of the random numbers in the the list: "))
+number_in_list=int(input("Enter the number of the elements in your list: "))
 user_list=[]
 for i in range (number_in_list):
-    n=random.randrange(100000)
+    n=random.randrange(1000)
     user_list.append(n)
 print("Your List: ", user_list)
